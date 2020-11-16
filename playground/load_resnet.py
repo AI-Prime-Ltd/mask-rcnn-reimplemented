fa7fd3fc69f4cbee631e9805e2a2628fbc99fca8
ziheng.zhang <ziheng.zhang@ai-prime.ai>
2020-11-16 19:42:15 +0800

    dummy commit

--- playground/load_resnet.py
+++ playground/load_resnet.py
@@ -5,11 +5,7 @@ from models.components.backbones.resnet import ResNet
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     resnet18 = ResNet.create_resnet18(pretrained=True)
-
     resnet50 = ResNet.create_resnet50(pretrained=True)
-
     resnet50d = ResNet.create_resnet50d(pretrained=True)
-
     resnext50d_32x4d = ResNet.create_resnext50d_32x4d(pretrained=True)
-
     seresnext50_32x4d = ResNet.create_seresnext50_32x4d(pretrained=True)
